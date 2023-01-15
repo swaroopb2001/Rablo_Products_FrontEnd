fc166d4f0ff344b7d992d4cf691ac239b71e7cfa
Akash-M1 <83898316+Akash-M1@users.noreply.github.com>
2023-01-15 19:39:02 +0530

    Adding deletion of products  and readme file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,34 @@
 var productElement = document.getElementById('products')
 
+
+window.addEventListener('click',(e)=>{
+    if(e.target.dataset.click == 'delete'){
+        deleteProduct(e.target.dataset.id);
+    }
+});
+
+function deleteProduct(id){
+    var res = {
+        prod_id:id
+    }
+    fetch("https://rabloproductsapiserver.up.railway.app/products",{method:"DELETE",headers: {'Content-Type': 'application/json'},body: JSON.stringify(res)})
+    .then(result=>result.json())
+    .then((data)=>{
+        displayToast("Product Deleted Successfully");
+        initilize();
+    })
+    .catch((err)=>{
+        console.log("Error in insertion");
+        displayToast("Error in insertion");
+    })
+}
+
+function displayToast(message){
+    alert(message);
+}
+
 function initilize(){
+    productElement.innerHTML='';
     fetch("https://rabloproductsapiserver.up.railway.app/products",{method:'GET'})
     .then(result=>result.json())
     .then((data)=>{
@@ -13,7 +41,12 @@ function initilize(){
                                         <h5 class="card-title">${resEle[i].name}</h5>
                                         <h6 class="card-subtitle mb-2 text-muted">${resEle[i].company}</h6>
                                         <p class="card-text">Price : ${"&#8377;"+resEle[i].price}</p>
-                                        <p class="card-text">Rating : ${resEle[i].rating["$numberDecimal"]}</p>
+                                        <p class="card-text">
+                                            Rating : ${resEle[i].rating["$numberDecimal"]}
+                                            <button type="button" class="btn btn-light deletebtn" data-click="delete" data-id=${resEle[i].prod_id}>
+                                                <i class="fa-regular fa-trash" data-click="delete" data-id=${resEle[i].prod_id}></i>
+                                            </button>
+                                        </p>
                                     </div>
                                 </div>`;
             productElement.append(divele);
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Front End Products Page and Add Product for Rablo
+
+
+## Link for hosted frontend page
+[Website Link](http://akashmalasetty.me/Rablo_Products_FrontEnd/)
+
+## Screenshot of Products page
+![Products Page](./assets/images/HomePage.png)
+
+## Screenshot of Add Product page
+![Add Product Page](./assets/images/AddProductsPage.png)
